6a9b50446fdf9a202ffd7ee5953c5e2505932d23
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2020-03-27 20:01:48 -0300

    incidents page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { View, Image, Text } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native'
 
 import logoImg from '../../assets/logo.png'
 
 import styles from './styles'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Incidents() {
+    const navigation = useNavigation()
+
+    const navigateToDetails = () => {
+        navigation.navigate('Details')
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -17,18 +25,36 @@ export default function Incidents() {
 
             <Text style={styles.title}>Bem-vindo!</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia</Text>
-            <View style={styles.incidentList}>
-                <View style={styles.incident}>
-                    <Text style={styles.incidentProperty}>ONG:</Text>
-                    <Text style={styles.incidentValue}>APAD</Text>
 
-                    <Text style={styles.incidentProperty}>CASO:</Text>
-                    <Text style={styles.incidentValue}>Cachorro atropelado</Text>
 
-                    <Text style={styles.incidentProperty}>VALOR:</Text>
-                    <Text style={styles.incidentValue}>R$ 120,00</Text>
-                </View>
-            </View>
+            <FlatList
+                data={[1, 2, 3]}
+                keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
+                renderItem={
+                    () => (
+                        <View style={styles.incidentList}>
+                            <View style={styles.incident}>
+                                <Text style={styles.incidentProperty}>ONG:</Text>
+                                <Text style={styles.incidentValue}>APAD</Text>
+
+                                <Text style={styles.incidentProperty}>CASO:</Text>
+                                <Text style={styles.incidentValue}>Cachorro atropelado</Text>
+
+                                <Text style={styles.incidentProperty}>VALOR:</Text>
+                                <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                                <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetails}>
+                                    <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                                    <Feather name="arrow-right" size={16} color="#E02141" />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    )
+                }
+                />
+
+            
         </View>
     )
 }
